d058e95eaccd11551584042023a157c9f717ded7
coon <coon@c-base.org>
2013-01-06 06:28:06 +0100

    FloppyOut Modul hinzugefügt

---
+++ FloppyOut.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+FlyFi - Floppy-Fidelity
+=======
+
+Created to fulfill all your floppy music needs.
+
+Created on Tue 06-01-2013_05:17:42+0100
+@author: Ricardo (XeN) Band <xen@c-base.org>,
+         Stephan (coon) Thiele <coon@c-base.org>
+
+    This file is part of FlyFi.
+
+    FlyFi is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FlyFi is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with FlyFi.  If not, see <http://www.gnu.org/licenses/>.
+
+    Diese Datei ist Teil von FlyFi.
+
+    FlyFi ist Freie Software: Sie können es unter den Bedingungen
+    der GNU General Public License, wie von der Free Software Foundation,
+    Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+    veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+    FlyFi wird in der Hoffnung, dass es nützlich sein wird, aber
+    OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+    Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+    Siehe die GNU General Public License für weitere Details.
+
+    Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+    Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Ricardo (XeN) Band <xen@c-base.org>, \
+              Stephan (coon) Thiele <coon@c-base.org>"
+__copyright__ = "Copyright (C) 2013 Ricardo Band, Stephan Thiele"
+__revision__ = "$Id$"
+__version__ = "0.1"
+
+
+import serial
+
+
+class FloppyOut():
+    def __init__(self):
+        self.ser = serial.Serial()
+
+    def initSerialCom(self, com_port):
+        self.ser.port = com_port 
+        self.ser.baudrate = 9600 
+        self.ser.parity = serial.PARITY_NONE
+        self.ser.stopbits = serial.STOPBITS_ONE
+        self.ser.bytesize = serial.EIGHTBITS
+        self.ser.open()
+    
+    def disconnectSerial(self, com_port):
+        self.ser.close()
+    
+    
+    def playTone(self, channel, frequency):
+        pass
+
+
+def main():
+    fl = FloppyOut()
+    fl.initSerialCom("/dev/ttyUSB3")
+    
+
+if __name__ == "__main__":
+    main()
