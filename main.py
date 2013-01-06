dd6b2dacb0b5561e77e61545952da654f0a9d1ab
coon <coon@c-base.org>
2013-01-06 05:57:33 +0100

    doc strings und init

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+FlyFi - Floppy-Fidelity
+"""
+
+#import 
 
 
 def main():
+    """
+    the main method. yeahah!
+    """
     print "blub!"
 
 
